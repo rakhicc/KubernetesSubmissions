cd342aa5394d13f5f7af964e30fbff867ffe49e6
Vinod Viswanathan <vinodviswanathan@Vinods-MacBook-Air.local>
2026-06-20 08:19:52 +0300

    todo_node changes

--- todo_node/README.md
+++ todo_node/README.md
@@ -1,6 +1,9 @@
 # second deployment to kubernetes
 a sample nodejs application
 execute the following command to deploy to kubernetes:
-kubectl apply -f deployment.yaml
+kubectl apply -f manifests/deployment.yaml
 then run kubectl get pods to get pod name.
 kubectl logs podname will give you the application logs.
+to run the application execute the following command
+# kubectl port-forward pod-name 8080:3000
+now check in browser http://localhost:8080
--- todo_node/index.js
+++ todo_node/index.js
@@ -3,7 +3,23 @@ const http = require('http');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Todo App');
+  if (req.url === '/' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(`
+      <html>
+        <head>
+          <title>Todo App</title>
+        </head>
+        <body>
+          <h1>Welcome to Todo App</h1>
+          <p>Server is running!</p>
+        </body>
+      </html>
+    `);
+  } else {
+    res.writeHead(404);
+    res.end('Not Found');
+  }
 });
 
 server.listen(PORT, () => {
