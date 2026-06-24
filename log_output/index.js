b29ff1a8b34797b0b5829a5a7f3ad2597410d4f5
Vinod Viswanathan <vinodviswanathan@Vinods-MacBook-Air.local>
2026-06-24 14:00:02 +0300

    configmap added to logoutput

--- log_output/index.js
+++ log_output/index.js
@@ -3,12 +3,13 @@ const http = require('http');
 const PORT = process.env.PORT || 3008;
 
 const randomString = Math.random().toString(36).substring(2);
-
+const fs = require('fs');
+const fileContentPath = '/etc/config/information.txt';
 // ✅ Call ping-pong service
 const getPingCount = () => {
     console.log("Fetching ping count from ping-pong service...");
   return new Promise((resolve) => {
-    http.get('http://ping-pong-svc/pings', (res) => {
+    http.get('http://ping-pong-svc:80/pings', (res) => {
       let data = '';
 
       res.on('data', chunk => data += chunk);
@@ -24,9 +25,23 @@ const server = http.createServer(async (req, res) => {
 
     const pingCount = await getPingCount();
 
+    let fileContent = "file not found";
+
+    try {
+      fileContent = fs.readFileSync(fileContentPath, 'utf8');
+    } catch (e) {}
+
+    const message = process.env.MESSAGE || "no message";
+
     res.writeHead(200);
-    res.end(`${timestamp}: ${randomString}\nPing / Pongs: ${pingCount}`);
-    return;
+    
+res.end(
+      `file content: ${fileContent}\n` +
+      `env variable: MESSAGE=${message}\n` +
+      `${timestamp}: ${randomString}\n` +
+      `Ping / Pongs: ${pingCount}`
+    );
+return;
   }
 
   res.writeHead(404);
