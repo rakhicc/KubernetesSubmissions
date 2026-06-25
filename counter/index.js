28545b942efd7777fb617eeeda9d21c0875b6168
Vinod Viswanathan <vinodviswanathan@Vinods-MacBook-Air.local>
2026-06-25 13:26:19 +0300

    postgres added to counter app using statefulset

--- counter/index.js
+++ counter/index.js
@@ -1,28 +1,48 @@
-const http = require('http');
+const { Pool } = require('pg');
+const express = require('express');
+const app = express();
 
-const PORT = process.env.PORT || 3007;
+const port = process.env.PORT || 3007;
 
-let counter = 0;
-
-const server = http.createServer((req, res) => {
-  if (req.url === '/pingpong') {
-    counter++;
-    res.writeHead(200);
-    res.end(`pong ${counter}`);
-    return;
-  }
-
-  // ✅ NEW endpoint for log-output
-  if (req.url === '/pings') {
-    res.writeHead(200);
-    res.end(counter.toString()); // ✅ return count only
-    return;
-  }
-
-  res.writeHead(404);
-  res.end('Not Found');
+// Database Connection configuration using Environment Variables
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
 });
 
-server.listen(PORT, () => {
-  console.log(`Ping-pong running on ${PORT}`);
+
+  app.get('/pingpong', async (req, res) => {
+  try {
+    console.log("Incrementing ping count in database...");
+    // 1. Fetch current count value
+    const result = await pool.query('SELECT count_value FROM counter_table WHERE id = 1;');
+    let currentCount = result.rows[0].count_value;
+
+    // 2. Increment it
+    currentCount++;
+
+    // 3. Update database
+    await pool.query('UPDATE counter_table SET count_value = $1 WHERE id = 1;', [currentCount]);
+
+    res.send(`pingpong ${currentCount}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Database connection or query error');
+  }
+});
+
+  // ✅ NEW endpoint for log-output
+  app.get('/pings', async (req, res) => {
+    try {
+    console.log("Fetching ping count from database...");
+      const result = await pool.query('SELECT count_value FROM counter_table WHERE id = 1;');
+      res.send(result.rows[0].count_value.toString());
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Database connection or query error');
+    }
+  });
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
