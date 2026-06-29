693be8f90e724941bb325c703aa02face9c30733
Vinod Viswanathan <vinodviswanathan@Vinods-MacBook-Air.local>
2026-06-29 14:44:06 +0300

    github workflow for todo app

--- todo_node/backend/index.js
+++ todo_node/backend/index.js
@@ -66,6 +66,7 @@ console.log("Inserted todo into database:", result.rows[0]);
 
 app.listen(PORT, () => {
   console.log('New logic initialized');
-  console.log('image resourse removed');
+  console.log('workflow initialized');
+  console.log('image resource removed');
   console.log(`Backend running on ${PORT}`);
 });
