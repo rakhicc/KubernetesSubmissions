6b4a0f50701a7a76ffb2a33c07c87c19497ac5c3
Vinod Viswanathan <vinodviswanathan@Vinods-MacBook-Air.local>
2026-06-30 08:53:31 +0300

    fix:set project id

--- todo_node/backend/index.js
+++ todo_node/backend/index.js
@@ -67,7 +67,7 @@ console.log("Inserted todo into database:", result.rows[0]);
 app.listen(PORT, () => {
   console.log('New logic initialized');
   console.log('workflow initialized');
-  console.log('workflow testing in progress');
+  console.log('workflow testing');
   console.log('image resource removed');
   console.log(`Backend running on ${PORT}`);
 });
