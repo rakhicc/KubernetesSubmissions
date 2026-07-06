cdbe27fabcceb206bf649d4d5f7c21bba92171a2
Vinod Viswanathan <vinodviswanathan@Vinods-MacBook-Air.local>
2026-07-06 08:44:24 +0300

    todo publishing check

--- todo_node/backend/nats.js
+++ todo_node/backend/nats.js
@@ -1,5 +1,3 @@
-// nats.js
-
 const { connect, StringCodec } = require('nats');
 
 let nc;
@@ -7,23 +5,41 @@ const sc = StringCodec();
 
 const connectNats = async () => {
   nc = await connect({
-    servers: process.env.NATS_URL || 'nats://my-nats.nats.svc.cluster.local:4222'
+    servers:
+      process.env.NATS_URL ||
+      'nats://my-nats.nats.svc.cluster.local:4222',
+
+    maxReconnectAttempts: -1
   });
 
   console.log('Connected to NATS');
+
+  nc.closed().then((err) => {
+    console.error('NATS connection closed:', err);
+  });
 };
 
 const publishEvent = async (payload) => {
+  try {
     console.log('Publishing:', payload);
-  if (!nc) {
-    console.log('NATS not connected');
-    return;
-  }
 
-  nc.publish(
-    'todo.events',
-    sc.encode(JSON.stringify(payload))
-  );
+    if (!nc) {
+      console.log('NATS not connected');
+      return;
+    }
+
+    console.log('NATS closed?', nc.isClosed());
+
+    nc.publish(
+      'todo.events',
+      sc.encode(JSON.stringify(payload))
+    );
+   await nc.flush();
+    console.log('Published successfully');
+
+  } catch (err) {
+    console.error('Publish failed:', err);
+  }
 };
 
 module.exports = {
